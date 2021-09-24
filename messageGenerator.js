136aad6d7f4b9a4cee2f7c9bd77a5ece71785e77
Jorrosfe <jorge.ros@gmail.com>
2021-09-24 13:17:40 +0200

    Added oject format to the message

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,7 +1,8 @@
-const sign = ['water', 'air', 'earth', 'fire', 'ice'];
-const having = ['good luck', 'too much estress', 'bad day', 'back luck', 'good feelings', 'faith'];
-const should = ['enjoy your life', 'expend more money', 'learn something new', 'go for adventure'];
-
-console.log('Your sign is: ' + sign[Math.floor(Math.random()*sign.length)]);
-console.log('Your are having ' + having[Math.floor(Math.random()*having.length)]);
-console.log('Your should ' + should[Math.floor(Math.random()*should.length)]);
+const message = {
+   sign : ['water', 'air', 'earth', 'fire', 'ice'],
+   having : ['good luck', 'too much estress', 'bad day', 'back luck', 'good feelings', 'faith'],
+   should : ['enjoy your life', 'expend more money', 'learn something new', 'go for adventure']
+}
+console.log('Your sign is: ' + message.sign[Math.floor(Math.random()*message.sign.length)]);
+console.log('Your are having ' + message.having[Math.floor(Math.random()*message.having.length)]);
+console.log('Your should ' + message.should[Math.floor(Math.random()*message.should.length)]);
